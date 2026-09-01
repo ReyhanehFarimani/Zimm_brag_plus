5f15d505ec069212a20a3fa8a067c2a93782526f
reyhaneh_macbook <reyhaneh.afghahi.farimani@univie.ac.at>
2026-09-01 17:10:37 +0200

    pivot move added so we have a faster way to capture the full conformational space.

--- src/MC.cpp
+++ src/MC.cpp
@@ -3,6 +3,7 @@
 #include "update_position.h"
 #include "update_state.h"
 #include "update_hinge.h"
+#include "update_pivot.h"
 
 MC::MC(const Input& in, Chain& chain)
     : in_(in), chain_(chain), rng_(in.seed) {}
@@ -22,10 +23,18 @@ void MC::sweep() {
         ++try_hinge_;
         if (try_hinge_move(chain_, pick(rng_), in_, rng_)) ++acc_hinge_;
     }
+    if (N > 2) {
+        std::uniform_int_distribution<int> pick_inner(1, N - 2);
+        for (int k = 0; k < in_.n_pivot; ++k) {
+            ++try_pivot_;
+            if (try_pivot_move(chain_, pick_inner(rng_), in_, rng_)) ++acc_pivot_;
+        }
+    }
 }
 
 void MC::reset_acceptance() {
     try_state_ = acc_state_ = 0;
     try_pos_   = acc_pos_   = 0;
     try_hinge_ = acc_hinge_ = 0;
+    try_pivot_ = acc_pivot_ = 0;
 }
